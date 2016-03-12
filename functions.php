862452739bc230ee51e704028c76b5b73923fe81
Ymazaki <camp.201602@gmail.com>
2016-03-12 10:30:18 +0900

    設定ファイル追加

---
+++ functions.php
@@ -0,0 +1,14 @@
+<?php
+function connectDtabase(){
+	try{
+		return new PDO(DSN,USER,PASSWORD);
+	}
+	catch(PDOException $e){
+		echo $e->getMessage();
+		exit;
+	}
+}
+
+function h($s){
+	return htmlspecialchars($s,ENT_QUOTES,"UTF-8");
+}
